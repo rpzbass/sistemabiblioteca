125638eb9e4662cc706d76354ab3a2b90ddae0d4
rpźbass <ronaldop.tech@gmail.com>
2021-04-16 12:20:28 -0300

    Add Exception [Exemplares Esgotados]

---
+++ sistemalivraria/src/main/java/mjv/devschool/sistemalivaria/service/exceptions/AccessComunicationError.java
@@ -0,0 +1,11 @@
+package mjv.devschool.sistemalivaria.service.exceptions;
+
+public class AccessComunicationError extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public AccessComunicationError(String msg) {
+		super(msg);
+	}
+	
+}
